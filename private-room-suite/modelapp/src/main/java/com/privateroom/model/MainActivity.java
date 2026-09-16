e9ab5bec6ce49ad9d78c1bdce03394dd6348ca2b
Tarayme <76899280+Tarayme@users.noreply.github.com>
2026-09-16 11:12:04 +0300

    Keep screen awake during calls

--- private-room-suite/modelapp/src/main/java/com/privateroom/model/MainActivity.java
+++ private-room-suite/modelapp/src/main/java/com/privateroom/model/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity {
 
   @Override public void onCreate(Bundle b){
     super.onCreate(b);
+    getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     web=new WebView(this);
     WebSettings s=web.getSettings();
